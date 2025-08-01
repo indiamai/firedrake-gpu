2fbc1dff1bc76c0ca02468d24b083c66190c472c
India Marsden <imm1117@ic.ac.uk>
2025-08-01 15:59:56 +0100

    indexing, determinant in kernel, test various cell block sizes

--- triton/minimal_triton.py
+++ triton/minimal_triton.py
@@ -4,23 +4,51 @@ import numpy as np
 import triton
 import triton.language as tl
 import torch
-
+import triton.tools as tl_tools
+import pdb
 DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 def next_power2(num):
     return int(np.power(2,np.ceil(np.log2(num))))
 
+# Ops for slicing (take/put) local tensor (extension to https://github.com/triton-lang/triton/pull/2715)
+# extension found https://github.com/triton-lang/triton/issues/656
+@triton.jit
+def _indicator_(n_dims: tl.constexpr, idx: tl.constexpr, pos: tl.constexpr, pos_dim: tl.constexpr):
+    tl.static_assert(idx < n_dims)
+    tl.static_assert(pos < pos_dim)
+    y = tl.arange(0, pos_dim)
+    y = tl.where(y==pos, 1, 0)
+    
+    for n in tl.static_range(0, n_dims):
+        if n != n_dims - 1 - idx:
+            y = tl.expand_dims(y, n)
+    return y
+
+@triton.jit
+def _take_slice_(x, n_dims: tl.constexpr, idx: tl.constexpr, pos: tl.constexpr, pos_dim:tl.constexpr, keep_dim: tl.constexpr = True):
+    ind = _indicator_(n_dims, idx, pos, pos_dim)
+    y = tl.sum(x * ind, n_dims - 1 - idx)
+    if keep_dim:
+        y = tl.expand_dims(y, n_dims - 1 - idx)
+
+    return y
+
+@triton.jit
+def _put_slice_(x, n_dims: tl.constexpr, idx: tl.constexpr, pos: tl.constexpr, pos_dim:tl.constexpr, input_slice):
+    ind = _indicator_(n_dims, idx, pos, pos_dim)
+    y = tl.where(ind==1, input_slice, x)
+    return y
 
 @triton.jit
 def add_kernel(cell_coords,
                t0,
                t1,
-               js_ptr,
                output_ptr,  # *Pointer* to output vector.
                c, 
                coords_c: tl.constexpr, coords_d: tl.constexpr,
                stride_coords_c, stride_coords_d, coords_size,
-               t0_d0: tl.constexpr, t0_d1: tl.constexpr, t0_stride0, t0_stride1, t0_size,
+               t0_d0: tl.constexpr, t0_d1: tl.constexpr, t0_stride0, t0_stride1, t0_size0, t0_size1,
                t1_dim: tl.constexpr, t1_stride, t1_size,
                BLOCK_SIZE_Q: tl.constexpr,  # Number of elements each program should process.
                BLOCK_SIZE_C: tl.constexpr,
@@ -31,55 +59,56 @@ def add_kernel(cell_coords,
     sub_offsets = tl.arange(0, coords_d)
     offsets =  cell_offsets[:, None]*stride_coords_c + sub_offsets[None, :]*stride_coords_d 
     
-    mask = offsets < coords_size 
-    coord_cells = tl.load(cell_coords + offsets, mask=mask)
-    
+    mask = offsets < (cell_offsets * stride_coords_c + coords_size)[:, None] 
+    coord_cells = tl.load(cell_coords + offsets, mask=mask, other=0)
+    coord_0 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 0, coords_d).reshape(BLOCK_SIZE_C)
+    coord_1 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 1, coords_d).reshape(BLOCK_SIZE_C)
+    coord_2 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 2, coords_d).reshape(BLOCK_SIZE_C)
+    coord_3 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 3, coords_d).reshape(BLOCK_SIZE_C)
+    coord_4 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 4, coords_d).reshape(BLOCK_SIZE_C)
+    coord_5 = _take_slice_(coord_cells, BLOCK_SIZE_C, 0, 5, coords_d).reshape(BLOCK_SIZE_C)
+    js = (-1 * coord_0 + coord_2)*(-1 * coord_1 + coord_5) - (-1 * coord_0 + coord_4)*(-1 * coord_1 + coord_3)
     t0_offsets0 = tl.arange(0, t0_d0)
     t0_offsets1 = tl.arange(0, t0_d1)
 
     t0_offsets =  t0_offsets0[:, None]*t0_stride0 + t0_offsets1[None, :]*t0_stride1
-    t0 = tl.load(t0 + t0_offsets, mask= t0_offsets < t0_size, other=0)
-
+    t0 = tl.load(t0 + t0_offsets, mask= t0_offsets < (t0_offsets0*t0_stride0 + t0_size1)[:, None], other=0)
     t1_offsets = tl.arange(0, t1_dim)
     t1 = tl.load(t1 + t1_offsets, mask= t1_offsets < t1_size, other=0)
-    js = tl.load(js_ptr + cell_offsets, mask = cell_offsets < c, other = 0)
-    is5 = tl.abs(js)
+ 
     res = tl.load(output_ptr + t1_offsets)
-    res = tl.sum(t0*(t1*is5)[:, None], 0)
-    tl.store(output_ptr + offsets, res[None, :], mask=mask)
+    pdb.set_trace()
+    res = tl.sum(t0*(tl.abs(js)[:, None]*t1)[:, :, None], 1)
+    tl.store(output_ptr + offsets, res, mask=mask)
 
 def add():
 
     data = np.load("tiny_data.npz")
-    derivs_gpu = torch.from_numpy(data["grad_basis"])
-    basis_funcs_gpu = torch.from_numpy(data["basis"])
-    cg_node_map_gpu = torch.from_numpy(data["cg_map"])
-    cg_data_gpu = cp.empty_like(data["empty_data"])
+    cg_data_gpu = cp.zeros_like(data["empty_data"])
+    cg_node_map_gpu = cp.array(data["cg_map"])
     coord_node_map_cp = cp.array(data["coords_map"])
     coord_data_cp = cp.array(data["coords"], dtype=cp.float32)
     cell_coords = cp.take(coord_data_cp, coord_node_map_cp, axis=0)
     cell_coords = cell_coords.reshape((cell_coords.shape[0], -1))
     coords_gpu = torch.from_numpy(cell_coords.get()).float().to(DEVICE)
-    js = (-1 * cell_coords[:, 0] + cell_coords[:,2])*(-1 * cell_coords[:, 1] + cell_coords[:,5]) - (-1 * cell_coords[:, 0] + cell_coords[:, 4])*(-1 * cell_coords[:,1] + cell_coords[:, 3])
-    weights_gpu = torch.from_numpy(data["weights"])
-    print(cell_coords)
+
     num_cells = coord_node_map_cp.shape[0] 
-    n_quads = basis_funcs_gpu.shape[-1] 
+    n_quads = 25 
     grid = lambda meta: (triton.cdiv(n_quads, meta['BLOCK_SIZE_Q']) * triton.cdiv(num_cells, meta['BLOCK_SIZE_C']), )
-    coords_size = len(cell_coords.flatten())
+
     output = torch.from_numpy(np.zeros((num_cells,6), dtype=np.float32)).to(DEVICE)
     t0=torch.from_numpy(np.array([[ 0.22222222,  0.44444444,  0.44444444, -0.11111111,  0.11111111,-0.11111111],
         [-0.11111111,0.11111111,0.44444444, -0.11111111,  0.44444444,0.22222222],
         [-0.11111111,  0.44444444,  0.11111111,  0.22222222,  0.44444444,-0.11111111]], dtype=np.float32)).to(DEVICE)
     t1= torch.from_numpy(np.array([0.16666667, 0.16666667, 0.16666667], dtype=np.float32)).to(DEVICE)
-    js = torch.from_numpy(js.get()).to(DEVICE)
-    add_kernel[grid](coords_gpu, t0, t1, js, output,
+
+    add_kernel[grid](coords_gpu, t0, t1, output,
                      num_cells, next_power2(cell_coords.shape[0]),
                      next_power2(cell_coords.shape[1]),
-                     coords_gpu.stride(0), coords_gpu.stride(1), len(cell_coords.flatten()), 
-                     next_power2(t0.shape[0]), next_power2(t0.shape[1]), t0.stride(0), t0.stride(1), len(t0.flatten()),
+                     coords_gpu.stride(0), coords_gpu.stride(1), len(cell_coords[0]), 
+                     next_power2(t0.shape[0]), next_power2(t0.shape[1]), t0.stride(0), t0.stride(1), t0.shape[0], t0.shape[1],
                      next_power2(len(t1)), t1.stride(0), len(t1), 
-                     BLOCK_SIZE_Q=5, BLOCK_SIZE_C=1)
+                     BLOCK_SIZE_Q=5, BLOCK_SIZE_C=16)
     torch.cuda.current_stream().synchronize()
     cpx.scatter_add(cg_data_gpu, cg_node_map_gpu, output)
     return cg_data_gpu
